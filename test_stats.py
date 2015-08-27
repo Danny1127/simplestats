c51312a703293205125eafce315f5f7974985236
Hikaru <zhangwendan@qq.com>
2015-08-27 13:46:46 -0500

    change test to assert_equal

--- test_stats.py
+++ test_stats.py
@@ -2,15 +2,18 @@ from stats import mean
 from nose.tools import assert_equal
 
 def test_mean():
-	assert(mean([2,4]) == 3)
+#	assert(mean([2,4]) == 3)
+	assert_equat(mean([2,4]),3)
 test_mean()
 
 def test_float_mean():
-	assert(mean([1,2]) == 1.5)
+#	assert(mean([1,2]) == 1.5)
+	assert_equal(mean([1,2]),1.5)
 #test_float_mean()
 
 def ex_test():
-	assert(mean([-2,2,4])==4.0/3)
+#	assert(mean([-2,2,4])==4.0/3)
+	assert_equal(mean([-2,2,4]),4./3)	
 #ex_test()
 
 
