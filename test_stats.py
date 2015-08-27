97a8e965b847ff06cf88a641dcd11a0dbf6fa18f
Hikaru <zhangwendan@qq.com>
2015-08-27 13:40:21 -0500

    final

--- test_stats.py
+++ test_stats.py
@@ -1,4 +1,6 @@
 from stats import mean
+from nose.tools import assert_equal
+
 def test_mean():
 	assert(mean([2,4]) == 3)
 test_mean()
@@ -9,6 +11,6 @@ def test_float_mean():
 
 def ex_test():
 	assert(mean([-2,2,4])==4.0/3)
-ex_test()
+#ex_test()
 
 
